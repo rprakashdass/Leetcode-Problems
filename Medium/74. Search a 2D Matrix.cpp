24003170ca5b810c24be4b7f0de7750766b761ef
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2024-10-14 20:45:58 +0530

    Update 74. Search a 2D Matrix.cpp

--- Medium/74. Search a 2D Matrix.cpp
+++ Medium/74. Search a 2D Matrix.cpp
@@ -18,3 +18,32 @@ public:
         return false;
     }
 };
+
+// Opmal Approach
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix[0].size();
+        int m = matrix.size();
+
+        int left = 0;
+        int right = n*m - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int r = mid / n;
+            int c = mid % n;
+            
+            if( matrix[r][c] == target)
+                return true;
+            else if ( target < matrix[r][c] )
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+
+        return false;
+    }
+};
