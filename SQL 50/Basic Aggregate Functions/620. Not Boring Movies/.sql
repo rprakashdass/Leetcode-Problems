85c99a2df6634d519aa30eb159cb181340e1239c
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2024-11-02 12:06:54 +0530

    Update .sql

--- SQL 50/Basic Aggregate Functions/620. Not Boring Movies/.sql
+++ SQL 50/Basic Aggregate Functions/620. Not Boring Movies/.sql
@@ -1,4 +1,11 @@
+-- Method 1
 SELECT *
 FROM Cinema
 WHERE id%2 != 0 AND description <> "boring"
+ORDER BY Rating DESC;
+
+-- Method 2
+SELECT *
+FROM Cinema
+WHERE id % 2 != 0 AND LOWER(description) NOT LIKE "%boring%"
 ORDER BY Rating DESC;
