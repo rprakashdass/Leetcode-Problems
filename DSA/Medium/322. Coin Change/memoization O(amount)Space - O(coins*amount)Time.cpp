ffb6ac28bfb3eaaa5ae4ead3c9b6171dd8530d3e
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-01-10 23:47:44 +0530

    Create memoization O(amount)Space - O(coins*amount)Time.cpp

---
+++ DSA/Medium/322. Coin Change/memoization O(amount)Space - O(coins*amount)Time.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int recur(vector<int>& coins, vector<int>& memo, int amount){
+        if(amount == 0) return 0;
+        if(amount < 0) return INT_MAX;
+        if(memo[amount] != -1) return memo[amount];
+        int minCount = INT_MAX;
+        for(size_t i = 0;i < coins.size();i++){
+            int res = recur(coins, memo, amount-coins[i]);
+            if(res != INT_MAX) minCount = min(minCount, res + 1);
+        }
+        return memo[amount] = minCount;
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>memo(amount+1, -1);
+        int res = recur(coins, memo, amount);
+        return (res != INT_MAX) ? res : -1;
+    }
+};
