f6e62bb0a678952c8128b0505ef2b8b9462eb4b7
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-01-24 17:43:26 +0530

    Update 7. Reverse Integer.cpp

--- DSA/Medium/7. Reverse Integer.cpp
+++ DSA/Medium/7. Reverse Integer.cpp
@@ -4,8 +4,10 @@ public:
         int reversed = 0;
         while(x != 0){
             int digit = x % 10;
-            if  ( reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 &&  digit > 7 ) ) return 0;
-            else if ( reversed < INT_MIN / 10 || (reversed == INT_MAX / 10 && digit < -8 ) ) return 0;
+            cout << digit << " ";
+            if( reversed > INT_MAX / 10 || reversed < INT_MIN / 10) {
+                return 0;
+            }
             reversed = ( reversed * 10 ) + digit;
             x /= 10;
         }
