9bae6ab18eca649b80a0a29890da5b437b8f762d
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-01-27 14:51:31 +0530

    Create bruite.cpp

---
+++ DSA/Medium/229. Majority Element II/bruite.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        unordered_map<int, int> mp;
+        for(int num: nums){
+            mp[num]++;
+        }
+        for(auto it: mp){
+            if(it.second > n/3) res.push_back(it.first); 
+        }
+        return res;
+    }
+};
