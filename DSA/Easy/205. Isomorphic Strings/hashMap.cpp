801c857317afc773129fc43ff8e927f1a3ad76b2
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-02-02 23:57:05 +0530

    Create hashMap.cpp

---
+++ DSA/Easy/205. Isomorphic Strings/hashMap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> mp1, mp2;
+        for(int i = 0;i < s.size();i++){
+            if((mp1.find(s[i]) != mp1.end() && mp1[s[i]] != t[i]) ||
+                (mp2.find(t[i]) != mp2.end() && mp2[t[i]] != s[i])){
+              return false;
+            }
+            mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
+        }
+        return true;
+    }
+};
