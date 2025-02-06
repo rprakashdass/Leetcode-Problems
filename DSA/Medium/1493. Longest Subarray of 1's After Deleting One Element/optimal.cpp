11fb2f795aa48485e24136ec3174bae7a6c46e41
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-02-06 12:20:18 +0530

    Create optimal.cpp

---
+++ DSA/Medium/1493. Longest Subarray of 1's After Deleting One Element/optimal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size(), zeros = 0, left = 0;
+        int count = 0;
+        for(int right = 0;right < n;right++){
+            if(nums[right] == 0) zeros++;
+            if(zeros > 1){
+                if(nums[left] == 0) zeros--;
+                left++;
+            } else {
+                count = max(count, right - left);
+            }
+        }
+        return count;
+    }
+};
