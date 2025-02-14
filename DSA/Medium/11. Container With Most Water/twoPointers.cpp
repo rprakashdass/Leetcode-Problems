3256a613b2fb953b372580160b14b64ceae184d6
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-02-14 14:29:56 +0530

    Create twoPointers.cpp

---
+++ DSA/Medium/11. Container With Most Water/twoPointers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i = 0, j = n-1;
+        int area = 0;
+        while(i < j){
+            area = max(area, (j - i) * min(height[i], height[j]));
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+        return area;
+    }
+};
