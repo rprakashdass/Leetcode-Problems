159a5d05799f155722161b19dc7871dc58326e91
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-02-17 21:34:10 +0530

    Create bfs.cpp

---
+++ DSA/Medium/199. Binary Tree Right Side View/bfs.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*> q;
+        vector<int> result;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node = nullptr;
+            for(int i =  q.size();i > 0;i--){
+                node = q.front();
+                q.pop();
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            result.push_back(node->val);
+        }
+        return result;
+    }
+};
