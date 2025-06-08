62a5b20115da93da70ce2b8dd2f1fad04f27c14d
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-06-08 17:05:30 +0530

    Update binarySearch.cpp

--- DSA/Medium/540. Single Element in a Sorted Array/binarySearch.cpp
+++ DSA/Medium/540. Single Element in a Sorted Array/binarySearch.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size()-1, mid = (left + right) / 2;
+        int left = 0, right = nums.size()-1;
         while(left < right) {
-            if(mid-1 >= 0 && mid+1 < nums.size() && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) {
-                return nums[mid];
-            } else if (mid%2 == 0){
-                if(mid < nums.size() && nums[mid] == nums[mid+1]) {
-                    left = mid+1;
-                } else {
-                    right = mid-1;
-                }
+            int mid = (left + right) / 2;
+            if(mid % 2 != 0) mid--;
+            if(nums[mid] == nums[mid+1]) {
+                left = mid + 2;
             } else {
-                if(nums[mid] == nums[mid-1]) {
-                    left = mid+1;
-                } else {
-                    right = mid-1;
-                }
+                right = mid;
             }
-            mid = (left + right) / 2;
         }
-        return nums[mid];
+        return nums[left];
     }
 };
