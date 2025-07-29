6e969a47f5e65d726ab892a06c3763c6d501699d
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-07-29 15:21:26 +0530

    Update and rename DSA/Medium/3. Longest Substring Without Repeating Characters.cpp to DSA/Medium/3. Longest Substring Without Repeating Characters/bruiteForce.cpp

---
+++ DSA/Medium/3. Longest Substring Without Repeating Characters/bruiteForce.cpp
@@ -0,0 +1,19 @@
+// Brute force O(N*N)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int maxCount = 0;
+        for(int i = 0;i < n;i++){
+            map<int, int> mp;
+            int count = 0;
+            for(int j = i;j < n;j++){
+                if(mp.find(s[j]) != mp.end()) break;
+                mp[s[j]]++;
+                count++;
+            }
+            maxCount = max(maxCount, count);
+        }
+        return maxCount;
+    }
+};
