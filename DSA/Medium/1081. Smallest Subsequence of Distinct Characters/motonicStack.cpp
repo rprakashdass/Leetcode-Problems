e8689301d93dc06e12cce1ab64451b9a67153cb0
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-10-18 23:27:27 +0530

    Create motonicStack.cpp

---
+++ DSA/Medium/1081. Smallest Subsequence of Distinct Characters/motonicStack.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> lastSeen(26, 0);
+        for(int i = 0;i < s.size();i++) lastSeen[s[i] - 'a'] = i;
+
+        vector<bool> visited(26, false);
+        stack<char> stq;
+
+        for (int i = 0;i < s.size();i++) {
+            if (visited[s[i] - 'a']) continue; // if visited, skip it to avoid multiple occurence
+            // pop out the largest char, which will occur later
+            while (!stq.empty() && s[i] < stq.top() && i < lastSeen[stq.top() - 'a']) {
+                visited[stq.top() - 'a'] = false;
+                stq.pop();
+            }
+            visited[s[i] - 'a'] = true;
+            stq.push(s[i]);
+        }
+
+        string result;
+        while(!stq.empty()) {
+            result = stq.top() + result;
+            stq.pop();
+        }
+
+        return result;
+    }
+};
