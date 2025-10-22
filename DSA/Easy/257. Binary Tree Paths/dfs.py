1da8a2f3f974d91b8e16cdd8b646ef5f8903c064
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2025-10-22 11:46:40 +0530

    Create dfs.py

---
+++ DSA/Easy/257. Binary Tree Paths/dfs.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        result = []
+
+        def dfs(root, path):
+            if not root:
+                return
+            
+            path.append(str(root.val))
+        
+            if not root.left and not root.right:
+                result.append("->".join(path))
+            else:        
+                dfs(root.left, path)
+                dfs(root.right, path)
+
+            path.pop()
+
+        dfs(root, [])
+        return result
